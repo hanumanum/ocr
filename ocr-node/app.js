6df336b8633bc3bd912041de9afcd0000de3cfe4
a.kolyan <a.kolyan@gratun.am>
2022-07-07 18:09:38 +0400

    decoderize tests, etc...

--- ocr-node/app.js
+++ ocr-node/app.js
@@ -1,17 +1,18 @@
 const imgPrc  = require("./prepare.image")
 const { createWorker } = require('tesseract.js');
 
-const worker = createWorker();
+const imgUrl = "52616e646f6d4956a0e862652c64136faa0f4ebf0f821d925a4acd5e21fec7b2_rotated_cropped.jpeg"
+const path = "/home/hanuman/Desktop/cards/" + imgUrl
 
+const worker = createWorker();
 
 (async () => {
     
-    const IMAGE_PATH = "images/2.jpg"
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
 
-    const { data: { text } } = await worker.recognize(IMAGE_PATH);
+    const { data: { text } } = await worker.recognize(path);
     console.log(text);
     await worker.terminate();
 })();
--- ocr-py/02.tesseract.py
+++ ocr-py/02.tesseract.py
@@ -1,7 +1,7 @@
 import pytesseract
 from PIL import Image
 
-img_file = "data/2_bw.jpg"
+img_file = "/home/hanuman/Desktop/cards/52616e646f6d4956a0e862652c64136faa0f4ebf0f821d925a4acd5e21fec7b2_rotated.jpeg"
 img = Image.open(img_file)
 
 orc_result = pytesseract.image_to_string(img)
